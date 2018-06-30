26022e426c8dc72f4988f1e839d71194ea92c505
Howard Feger <hfeger4@gmail.com>
2018-06-30 15:05:55 -0700

    add to tasks controller, ability to create new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,21 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ["Clean garage", "Kill spiders", "Play vidya games"]
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  private
+  #whitelisting parameters
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
