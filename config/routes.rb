67faa8469b34d5bfcc41a1b2e81f434b6681cea5
Howard Feger <hfeger4@gmail.com>
2018-07-01 12:10:28 -0700

    add ability to sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
+
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+
+
   resources :users
+  get 'signup', to: 'users#new'
   get 'tasks/index'
 
   get 'tasks/new'
